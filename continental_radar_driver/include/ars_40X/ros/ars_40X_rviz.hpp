3979ee06ef5142181adcca5bd1867d78022dc0bb
GoodarzMehr <goodarzm@vt.edu>
2022-05-11 02:07:22 +0000

    Modified RViz visualization

--- continental_radar_driver/include/ars_40X/ros/ars_40X_rviz.hpp
+++ continental_radar_driver/include/ars_40X/ros/ars_40X_rviz.hpp
@@ -1,56 +1,58 @@
-//
-// Created by shivesh on 9/13/19.
-//
-
 #ifndef ARS_40X_ARS_40X_RVIZ_HPP
 #define ARS_40X_ARS_40X_RVIZ_HPP
 
 #include <ros/ros.h>
 
 namespace ars_40X {
-enum {
-  POINT,
-  CAR,
-  TRUCK,
-  PEDESTRIAN,
-  MOTORCYCLE,
-  BICYCLE,
-  WIDE,
-  RESERVED
-};
 
-enum {
-  INVALID,
-  PERCENT_25,
-  PERCENT_50,
-  PERCENT_75,
-  PERCENT_90,
-  PERCENT_99,
-  PERCENT_99_9,
-  PERCENT_100
-};
+    enum {
+        POINT,
+        CAR,
+        TRUCK,
+        PEDESTRIAN,
+        MOTORCYCLE,
+        BICYCLE,
+        WIDE,
+        RESERVED
+    };
 
-class ContinentalRadarRViz {
- public:
-  ContinentalRadarRViz();
+    enum {
+        INVALID,
+        PERCENT_25,
+        PERCENT_50,
+        PERCENT_75,
+        PERCENT_90,
+        PERCENT_99,
+        PERCENT_99_9,
+        PERCENT_100
+    };
 
-  ~ContinentalRadarRViz();
+    class ContinentalRadarRViz {
+        public:
+            ContinentalRadarRViz();
 
- private:
-  void clusters_callback(ars_40X::ClusterList cluster_list);
+            ~ContinentalRadarRViz();
 
-  void objects_callback(ars_40X::ObjectList object_list);
+        private:
+            void clusters_callback(ars_40X::ClusterList cluster_list);
 
-  ros::Publisher clusters_pub_;
+            void objects_callback(ars_40X::ObjectList object_list);
 
-  ros::Publisher objects_pub_;
+            ros::Publisher clusters_pub_;
 
-  ros::Publisher velocity_pub_;
+            ros::Publisher objects_pub_;
 
-  ros::Subscriber clusters_sub_;
+            ros::Publisher text_pub_;
 
-  ros::Subscriber objects_sub_;
-};
+            ros::Subscriber clusters_sub_;
+
+            ros::Subscriber objects_sub_;
+
+            const std::string dynamic_property[8] = {"Moving", "Stationary", "Oncoming",
+                "Stationary candidate", "Unknown", "Crossing stationary", "Crossing moving", "Stopped"};
+            const std::string class_type[8] = {"point", "car", "truck", "not in use",
+                "motorcycle", "bicycle", "wide", "reserved"};
+    };
 }
 
 #endif //ARS_40X_ARS_40X_RVIZ_HPP
--- continental_radar_driver/src/ros/ars_40X_rviz.cpp
+++ continental_radar_driver/src/ros/ars_40X_rviz.cpp
@@ -1,7 +1,3 @@
-
-// Created by shivesh on 9/18/19.
-//
-
 #include <ros/ros.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
@@ -11,266 +7,293 @@
 #include "ars_40X/ros/ars_40X_rviz.hpp"
 
 namespace ars_40X {
-ContinentalRadarRViz::ContinentalRadarRViz() {
-  ros::NodeHandle nh;
-  clusters_pub_ = nh.advertise<visualization_msgs::MarkerArray>("visualize_clusters", 50);
-  objects_pub_ = nh.advertise<visualization_msgs::MarkerArray>("visualize_objects", 50);
-  velocity_pub_ = nh.advertise<visualization_msgs::MarkerArray>("visualize_velocity", 50);
-  clusters_sub_ =
-      nh.subscribe("ars_40X/clusters", 50, &ContinentalRadarRViz::clusters_callback, this);
-  objects_sub_ =
-      nh.subscribe("ars_40X/objects", 50, &ContinentalRadarRViz::objects_callback, this);
-}
 
-ContinentalRadarRViz::~ContinentalRadarRViz() {
-}
+    ContinentalRadarRViz::ContinentalRadarRViz() {
+        ros::NodeHandle nh;
 
-void ContinentalRadarRViz::clusters_callback(ars_40X::ClusterList cluster_list) {
-  visualization_msgs::MarkerArray marker_array;
-  for (auto cluster : cluster_list.clusters) {
-    visualization_msgs::Marker marker;
-    marker.type = visualization_msgs::Marker::POINTS;
-    marker.header.frame_id = cluster_list.header.frame_id;
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.header.stamp = cluster_list.header.stamp;
-    marker.id = cluster.id;
-    marker.points.push_back(cluster.position.pose.position);
-    switch (cluster.prob_of_exist) {
-      case INVALID: {
-        marker.color.r = 0.0f;
-        marker.color.g = 0.0f;
-        marker.color.b = 0.0f;
-        marker.ns = "Invalid";
-        break;
-      }
+        // Define publishers.
+        clusters_pub_ = nh.advertise<visualization_msgs::MarkerArray>("visualize_clusters", 10);
+        objects_pub_ = nh.advertise<visualization_msgs::MarkerArray>("visualize_objects", 10);
+        text_pub_ = nh.advertise<visualization_msgs::MarkerArray>("visualize_text", 10);
 
-      case PERCENT_25: {
-        marker.color.r = 0.0f;
-        marker.color.g = 0.0f;
-        marker.color.b = 1.0f;
-        marker.ns = "25%";
-        break;
-      }
-
-      case PERCENT_50: {
-        marker.color.r = 0.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 0.0f;
-        marker.ns = "50%";
-        break;
-      }
-
-      case PERCENT_75: {
-        marker.color.r = 0.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 1.0f;
-        marker.ns = "75%";
-        break;
-      }
-
-      case PERCENT_90: {
-        marker.color.r = 1.0f;
-        marker.color.g = 0.0f;
-        marker.color.b = 0.0f;
-        marker.ns = "90%";
-        break;
-      }
-
-      case PERCENT_99: {
-        marker.color.r = 1.0f;
-        marker.color.g = 0.0f;
-        marker.color.b = 1.0f;
-        marker.ns = "99%";
-        break;
-      }
-
-      case PERCENT_99_9: {
-        marker.color.r = 1.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 0.0f;
-        marker.ns = "99.9%";
-        break;
-      }
-
-      case PERCENT_100: {
-        marker.color.r = 1.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 1.0f;
-        marker.ns = "100%";
-        break;
-      }
-    }
-    marker.scale.x = 0.1;
-    marker.scale.y = 0.1;
-    marker.color.a = 1.0;
-    marker.lifetime.fromSec(0.1);
-    marker_array.markers.push_back(marker);
-  }
-  clusters_pub_.publish(marker_array);
-}
-
-void ContinentalRadarRViz::objects_callback(ars_40X::ObjectList object_list) {
-  visualization_msgs::MarkerArray marker_array, velocity_array;
-  for (auto object : object_list.objects) {
-    visualization_msgs::Marker marker;
-    marker.type = visualization_msgs::Marker::LINE_STRIP;
-    marker.header.frame_id = object_list.header.frame_id;
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.header.stamp = object_list.header.stamp;
-    marker.id = object.id;
-    geometry_msgs::Point pos1, pos2, pos3, pos4;
-    tf2::Quaternion q;
-    q.setValue(
-        object.position.pose.orientation.x,
-        object.position.pose.orientation.y,
-        object.position.pose.orientation.z,
-        object.position.pose.orientation.w);
-    tf2::Matrix3x3 m(q);
-    double roll, pitch, yaw;
-    m.getRPY(roll, pitch, yaw);
-    if (isnan(yaw)) {
-      continue;
-    }
-    marker.pose = object.position.pose;
-    pos1.x = object.length / 2;
-    pos1.y = object.width / 2;
-    pos2.x = object.length / 2;
-    pos2.y = -object.width / 2;
-    pos3.x = -object.length / 2;
-    pos3.y = -object.width / 2;
-    pos4.x = -object.length / 2;
-    pos4.y = object.width / 2;
-    marker.points.push_back(pos1);
-    marker.points.push_back(pos2);
-    marker.points.push_back(pos3);
-    marker.points.push_back(pos4);
-    marker.points.push_back(pos1);
-    marker.scale.x = 0.1;
-
-    switch (object.class_type) {
-      case POINT: {
-        marker.color.r = 0.0f;
-        marker.color.g = 0.0f;
-        marker.color.b = 0.0f;
-        break;
-      }
-
-      case CAR: {
-        marker.color.r = 0.0f;
-        marker.color.g = 0.0f;
-        marker.color.b = 1.0f;
-        break;
-      }
-
-      case TRUCK: {
-        marker.color.r = 0.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 0.0f;
-        break;
-      }
-
-      case PEDESTRIAN: {
-        marker.color.r = 0.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 1.0f;
-        break;
-      }
-
-      case MOTORCYCLE: {
-        marker.color.r = 1.0f;
-        marker.color.g = 0.0f;
-        marker.color.b = 0.0f;
-        break;
-      }
-
-      case BICYCLE: {
-        marker.color.r = 1.0f;
-        marker.color.g = 0.0f;
-        marker.color.b = 1.0f;
-        break;
-      }
-
-      case WIDE: {
-        marker.color.r = 1.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 0.0f;
-        break;
-      }
-
-      case RESERVED: {
-        marker.color.r = 1.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 1.0f;
-        break;
-      }
+        // Define subscribers.
+        clusters_sub_ =
+            nh.subscribe("ars_40X/clusters", 10, &ContinentalRadarRViz::clusters_callback, this);
+        objects_sub_ =
+            nh.subscribe("ars_40X/objects", 10, &ContinentalRadarRViz::objects_callback, this);
     }
 
-    switch (object.prob_of_exist) {
-      case INVALID: {
-        marker.ns = "Invalid";
-        break;
-      }
-
-      case PERCENT_25: {
-        marker.ns = "25%";
-        break;
-      }
-
-      case PERCENT_50: {
-        marker.ns = "50%";
-        break;
-      }
-
-      case PERCENT_75: {
-        marker.ns = "75%";
-        break;
-      }
-
-      case PERCENT_90: {
-        marker.ns = "90%";
-        break;
-      }
-
-      case PERCENT_99: {
-        marker.ns = "99%";
-        break;
-      }
-
-      case PERCENT_99_9: {
-        marker.ns = "99.9%";
-        break;
-      }
-
-      case PERCENT_100: {
-        marker.ns = "100%";
-        break;
-      }
+    ContinentalRadarRViz::~ContinentalRadarRViz() {
     }
-    marker.color.a = 1.0;
-    marker.lifetime.fromSec(0.1);
-    marker_array.markers.push_back(marker);
 
-    visualization_msgs::Marker velocity;
-    velocity.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-    velocity.header.frame_id = object_list.header.frame_id;
-    velocity.action = visualization_msgs::Marker::ADD;
-    velocity.header.stamp = object_list.header.stamp;
-    velocity.id = object.id;
-    velocity.text = std::to_string(hypot(object.relative_velocity.twist.linear.x,
-                                         object.relative_velocity.twist.linear.y));
-    velocity.pose = object.position.pose;
-    velocity.scale.z = 1;
-    velocity.color.a = 1;
+    void ContinentalRadarRViz::clusters_callback(ars_40X::ClusterList cluster_list) {
+        
+        visualization_msgs::MarkerArray marker_array;
 
-    velocity_array.markers.push_back(velocity);
-  }
-  objects_pub_.publish(marker_array);
-  velocity_pub_.publish(velocity_array);
-}
+        for (auto cluster : cluster_list.clusters) {
+            
+            visualization_msgs::Marker marker;
+
+            marker.type = visualization_msgs::Marker::CUBE;
+            marker.action = visualization_msgs::Marker::ADD;
+
+            marker.header.frame_id = cluster_list.header.frame_id;
+            marker.header.stamp = cluster_list.header.stamp;
+            marker.id = cluster.id;
+
+            marker.pose = cluster.position.pose;
+
+            marker.color.r = 0.0;
+            marker.color.g = 1.0;
+            marker.color.b = 0.4;
+
+            switch (cluster.prob_of_exist) {
+                case INVALID: {
+                    marker.color.a = 0.1;
+                    marker.ns = "Invalid";
+                    break;
+                }
+
+                case PERCENT_25: {
+                    marker.color.a = 0.25;
+                    marker.ns = "25%";
+                    break;
+                }
+
+                case PERCENT_50: {
+                    marker.color.a = 0.5;
+                    marker.ns = "50%";
+                    break;
+                }
+
+                case PERCENT_75: {
+                    marker.color.a = 0.75;
+                    marker.ns = "75%";
+                    break;
+                }
+
+                case PERCENT_90: {
+                    marker.color.a = 0.9;
+                    marker.ns = "90%";
+                    break;
+                }
+
+                case PERCENT_99: {
+                    marker.color.a = 0.99;
+                    marker.ns = "99%";
+                    break;
+                }
+
+                case PERCENT_99_9: {
+                    marker.color.a = 0.999;
+                    marker.ns = "99.9%";
+                    break;
+                }
+
+                case PERCENT_100: {
+                    marker.color.a = 1.0;
+                    marker.ns = "100%";
+                    break;
+                }
+            }
+
+            marker.scale.x = 0.4;
+            marker.scale.y = 0.4;
+            marker.scale.z = 0.4;
+
+            marker.lifetime.fromSec(1 / 12.9);
+
+            marker_array.markers.push_back(marker);
+        }
+
+        clusters_pub_.publish(marker_array);
+    }
+
+    void ContinentalRadarRViz::objects_callback(ars_40X::ObjectList object_list) {
+        
+        visualization_msgs::MarkerArray marker_array, text_array;
+
+        for (auto object : object_list.objects) {
+            
+            visualization_msgs::Marker marker;
+
+            if (object.class_type == POINT)
+                marker.type = visualization_msgs::Marker::SPHERE;
+            else
+                marker.type = visualization_msgs::Marker::CUBE;
+            
+            marker.action = visualization_msgs::Marker::ADD;
+
+            marker.header.frame_id = object_list.header.frame_id;
+            marker.header.stamp = object_list.header.stamp;
+            marker.id = object.id;
+
+            marker.pose = object.position.pose;
+
+            switch (object.class_type) {
+                case POINT: {
+                    marker.color.r = 0.6;
+                    marker.color.g = 0.6;
+                    marker.color.b = 0.6;
+                    break;
+                }
+
+                case CAR: {
+                    marker.color.r = 0.2;
+                    marker.color.g = 0.2;
+                    marker.color.b = 0.8;
+                    break;
+                }
+
+                case TRUCK: {
+                    marker.color.r = 0.2;
+                    marker.color.g = 0.8;
+                    marker.color.b = 0.2;
+                    break;
+                }
+
+                case PEDESTRIAN: {
+                    marker.color.r = 0.2;
+                    marker.color.g = 0.8;
+                    marker.color.b = 0.8;
+                    break;
+                }
+
+                case MOTORCYCLE: {
+                    marker.color.r = 0.8;
+                    marker.color.g = 0.2;
+                    marker.color.b = 0.2;
+                    break;
+                }
+
+                case BICYCLE: {
+                    marker.color.r = 0.8;
+                    marker.color.g = 0.2;
+                    marker.color.b = 0.8;
+                    break;
+                }
+
+                case WIDE: {
+                    marker.color.r = 0.8;
+                    marker.color.g = 0.8;
+                    marker.color.b = 0.2;
+                    break;
+                }
+
+                case RESERVED: {
+                    marker.color.r = 0.8;
+                    marker.color.g = 0.8;
+                    marker.color.b = 0.8;
+                    break;
+                }
+            }
+
+            switch (object.prob_of_exist) {
+                case INVALID: {
+                    marker.color.a = 0.1;
+                    marker.ns = "Invalid";
+                    break;
+                }
+
+                case PERCENT_25: {
+                    marker.color.a = 0.25;
+                    marker.ns = "25%";
+                    break;
+                }
+
+                case PERCENT_50: {
+                    marker.color.a = 0.5;
+                    marker.ns = "50%";
+                    break;
+                }
+
+                case PERCENT_75: {
+                    marker.color.a = 0.75;
+                    marker.ns = "75%";
+                    break;
+                }
+
+                case PERCENT_90: {
+                    marker.color.a = 0.9;
+                    marker.ns = "90%";
+                    break;
+                }
+
+                case PERCENT_99: {
+                    marker.color.a = 0.99;
+                    marker.ns = "99%";
+                    break;
+                }
+
+                case PERCENT_99_9: {
+                    marker.color.a = 0.999;
+                    marker.ns = "99.9%";
+                    break;
+                }
+
+                case PERCENT_100: {
+                    marker.color.a = 1.0;
+                    marker.ns = "100%";
+                    break;
+                }
+            }
+
+            marker.scale.x = object.length;
+            marker.scale.y = object.width;
+            marker.scale.z = 1.0;
+
+            marker.lifetime.fromSec(1 / 12.9);
+
+            marker_array.markers.push_back(marker);
+
+            visualization_msgs::Marker text;
+
+            text.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+            text.action = visualization_msgs::Marker::ADD;
+
+            text.header.frame_id = object_list.header.frame_id;
+            text.header.stamp = object_list.header.stamp;
+            text.id = object.id;
+
+            int precisionVal = 1;
+
+            text.text = "Object " + std::to_string(object.id) + "\n"
+                + "LongD:" + std::to_string(object.position.pose.position.x).substr(0,
+                    std::to_string(object.position.pose.position.x).find(".") + precisionVal + 1)
+                    + "m "
+                + "LatD:" + std::to_string(object.position.pose.position.y).substr(0,
+                    std::to_string(object.position.pose.position.y).find(".") + precisionVal + 1)
+                    + "m\n"
+                + "RLongV:" + std::to_string(object.relative_velocity.twist.linear.x).substr(0,
+                    std::to_string(object.relative_velocity.twist.linear.x).find(".")
+                    + precisionVal + 1) + "m/s " 
+                + "RLatV:" + std::to_string(object.relative_velocity.twist.linear.y).substr(0,
+                    std::to_string(object.relative_velocity.twist.linear.y).find(".")
+                    + precisionVal + 1) + "m/s\n"
+                + dynamic_property[object.dynamic_property] + " " + class_type[object.class_type];
+
+            text.pose = object.position.pose;
+            text.pose.position.z = 2.0;
+
+            text.scale.z = 0.4;
+
+            text.color.r = 1.0;
+            text.color.g = 1.0;
+            text.color.b = 1.0;
+            text.color.a = 1.0;
+
+            text.lifetime.fromSec(1 / 12.9);
+
+            text_array.markers.push_back(text);
+        }
+
+        objects_pub_.publish(marker_array);
+        text_pub_.publish(text_array);
+    }
 }
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "ars_40X_rviz");
-  ars_40X::ContinentalRadarRViz ars_40X_rviz;
-  ros::spin();
+    ros::init(argc, argv, "ars_40X_rviz");
+    ars_40X::ContinentalRadarRViz ars_40X_rviz;
+    ros::spin();
 }
